8014d5541f3a16dc3ba4d497523df092fd6a7c95
Justin W <btxbaky@gmail.com>
2018-03-15 08:06:01 +0900

    Update InsertApp.php

--- InsertApp.php
+++ InsertApp.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Create Accont </title>
+        <title>Create Account</title>
     </head>
     <body>
         <?php
